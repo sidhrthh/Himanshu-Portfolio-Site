600ecac1fe550fde754efd1254721542d6d7f194
Sidharth pandro <sidharthpandro50@gmail.com>
2023-11-14 14:08:40 +0530

    Adds files.

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-const toggleBtn = document.querySelector(".toggle-btn a")
-const toggleBtnIcon = document.querySelector(".toggle.btn")
-const dropDownMenu = document.querySelector(".dropdown-menu")
-
-toggleBtn.onClick = function() {
-    dropDownMenu.classList.toggle('open')
-
-}
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector('.nav-bar'); 
+hamburger.addEventListener('click', () => { 
+  navMenu.classList.toggle('hide'); 
+});
